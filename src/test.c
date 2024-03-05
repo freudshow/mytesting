4f67aad22fa1f9303e834b8f224a18ea4b57eee8
s_baoshan <s_baoshan@163.com>
2024-03-05 11:35:25 +0800

    test char to short

--- src/test.c
+++ src/test.c
@@ -19,8 +19,18 @@ int main(int argc, char **argv)
 //    testInitDataList();
 //    testtcp();
 //    OLED_test();
-//	testmydb();
-    testjson();
+//	  testmydb();
+//    testjson();
+
+    int i = 0;
+    short int j[3] = { 0 };
+    short int *pint = j;
+    unsigned char buff[] = { 0x00, 0x64, 0x00, 0x70, 0x00, 0x91 };
+    for(i =0;i<3;i++)
+    {
+        *(pint + i) = ((*(char*) (buff + 2 * i) & 0xff) << 8) | (*(char*) (buff + 1 + 2 * i));
+        printf("%d,%4x,%d\n",j[i],j[i],*(pint + i));
+    }
 
     return 0;
 }
